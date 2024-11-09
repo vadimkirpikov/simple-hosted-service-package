5199e74c8de34889c6519fa383b32c65339b9f3b
Vadim Kirpikov <vadim.kir13@rambler.ru>
2024-11-09 16:47:58 +0400

    Initial commit

---
+++ ExtentionForAddingSimpleHostedService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleHostedService;
+
+public static class ExtensionForAddingSimpleHostedService
+{
+    /// <summary>
+    /// Extension method for adding
+    /// simplified hosted service
+    /// </summary>
+    /// <param name="serviceCollection">Service collection</param>
+    /// <param name="executeTask">The task for execution</param>
+    /// <returns></returns>
+    public static IServiceCollection AddSimpleHostedService(this IServiceCollection serviceCollection,
+        Func<CancellationToken, Task> executeTask)
+    {
+        serviceCollection.AddHostedService<SimplifiedHostedService>(sp => new SimplifiedHostedService(executeTask));
+        return serviceCollection;
+    }
+}
---
+++ SimplifiedHostedService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+namespace SimpleHostedService;
+
+/// <summary>
+/// This is simplified hosted service
+/// with one task for execution
+/// It can be easily added in service
+/// collection of application
+/// </summary>
+public class SimplifiedHostedService : IHostedService
+{
+    private CancellationTokenSource? _cts;
+    private Func<CancellationToken, Task> _executeTask;
+
+    /// <summary>
+    /// Initialize
+    /// </summary>
+    /// <param name="executeTask">The task for executing</param>
+    public SimplifiedHostedService(Func<CancellationToken, Task> executeTask)
+    {
+        _executeTask = executeTask??throw new ArgumentException("The executed task can not be null");
+    }
+
+    /// <summary>
+    /// Starts executed task
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        return Task.Run(() => _executeTask(_cts.Token), cancellationToken);
+    }
+
+    /// <summary>
+    /// Stops executed task
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cts!.Cancel();
+        return Task.CompletedTask;
+    }
+}
